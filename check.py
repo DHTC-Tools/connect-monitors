06fc174fef799da5fcfb6c878f89d242f6908e43
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-05-14 12:51:18 -0500

    Add support for administrative messages and indicate when tests were run.

--- check.py
+++ check.py
@@ -2,10 +2,14 @@
 
 import hashlib
 import sys
+import os
+import time
 
 import jinja2
 import requests
 import paramiko
+
+ADMIN_MESG_LOCATION = "/tmp/admin_message"
 
 def test_login(test_name, host, user ):
   response = [test_name, "OK", ""]
@@ -55,10 +59,12 @@ def test_xroot(test_name, uri, sha1):
 def run_tests():
   http_results = []
   http_hosts = [['Stash HTTP test', 
-                 'http://stash.osgconnect.net/keys/cern.ch.pub',
+                 'http://stash.osgconnect.net/keys/cern.ch.pub', 
                  '5b83bedef4c7ba38520d7e1b764f0cbc28527fb9']]
   ssh_results = []
-  ssh_hosts = [['OSG Connect login', 'login.osgconnect.net', 'sthapa']]
+  ssh_hosts = [['OSG Connect login', 'login.osgconnect.net', 'sthapa'],
+               ['ATLAS Connect login', 'login.usatlas.org', 'sthapa']]
+  
   #xrdcp_hosts = [['FAXBOX', 'xrootd://faxbox.atlasconnect.net', 'sha1']]
   for host in http_hosts:
     http_results.append(test_download(*host))
@@ -66,9 +72,14 @@ def run_tests():
     ssh_results.append(test_login(*host))
   env = jinja2.Environment(loader=jinja2.FileSystemLoader( searchpath="." ))
   template = env.get_template('templates/status.html')
+  admin_mesg = None
+  if os.path.isfile(ADMIN_MESG_LOCATION):
+    admin_mesg = open(ADMIN_MESG_LOCATION).read()
   print template.render(http_results = http_results,
                         ssh_results = ssh_results,
-                        xrootd_results = [])
+                        xrootd_results = [],
+                        admin_mesg = admin_mesg,
+                        time = time.asctime())
 if __name__ == "__main__":
    run_tests()
    sys.exit(0)
